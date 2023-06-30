2de004e57601d7ff176311043e20727fa3cfc2e2
SamKynaston <samuelkynno@gmail.com>
2023-06-30 01:27:44 -0700

    components and router config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
+//Modules
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//Styles
+import './index.css';
+
+//Pages
+import Catalogue from './Catalogue';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Catalogue/>} />
+      </Routes>
+    </Router>
+  )
+}
