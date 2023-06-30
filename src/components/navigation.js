b80c96cca475ef039ff31cc7480e1ae29fb502bc
SamKynaston <samuelkynno@gmail.com>
2023-06-30 06:07:26 -0700

    logo, navbar

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,11 +1,17 @@
 import "./styles/nav.css"
 import { Link } from "react-router-dom"
+import Logo from "../img/Cats4Life.png"
 
 const Navigation = ({getCart}) => {
     console.log(getCart)
     return (
         <nav>
-            <Link to="/cart">{getCart.length}</Link>
+            <span className="left">
+                <Link className="nav-item" to="/"><img src={Logo} height={25}/></Link>
+            </span>
+            <span className="right">
+                <Link className="nav-item" to="/cart">{getCart.length}</Link>
+            </span>
         </nav>
     )
 }
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,6 +1,14 @@
 import CartList from "../components/cart";
 
 const Cart = ({getCart}) => {
+    if (!getCart || getCart.length < 1) {
+        return (
+            <>
+                <h1>This is looking a little empty...</h1>
+            </>
+        );
+    }
+
     return (
         <>
             <CartList cart={getCart}/>
