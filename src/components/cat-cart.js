470318ae267cd973c667bb0ed97c16f63de11772
SamKynaston <samuelkynno@gmail.com>
2023-06-30 04:47:51 -0700

    cart system

---
+++ src/components/cat-cart.js
@@ -0,0 +1,14 @@
+import "./styles/cat.css"
+
+const CatCart = ({name, image, breed, cost}) => {
+    return (
+        <div className="item">
+            <h1>{name}</h1>
+            <img src={image} />
+            <h4>{breed}</h4>
+            <p>£{cost}</p>
+        </div>
+    )
+}
+
+export default Cat
